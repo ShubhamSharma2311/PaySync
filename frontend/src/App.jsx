a8ad40ea6ba9a30299707ffcc2c71a0550eb2d95
ShubhamSharma2311 <sharma.2022ug2068@iiitranchi.ac.in>
2025-02-23 12:31:08 +0530

    commit after creating git routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
 
   return (
     <div>
-        Hello world
+        <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element = {<Signup/>} />
+          <Route path='/signin' element = {<Signin/>} />
+          <Route path='/dashboard' element = {<Dashboard/>} />
+          <Route path='/send' element = {<Send/>} />
+        </Routes>
+        </BrowserRouter>
     </div>
   )
 }
