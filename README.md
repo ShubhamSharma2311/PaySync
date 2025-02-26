db07c32d59c68cfdcc9b2a20f801b4df8965fcc4
ShubhamSharma2311 <sharma.2022ug2068@iiitranchi.ac.in>
2025-02-26 11:04:46 +0530

    final commit before push

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 
 ## Build a basic version of PayTM
+=======
+# PaySync
+>>>>>>> c4b4250cbdc597021002069be39efa8b69279ebe
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom'
 import SignUp from './pages/signup'
 import SignIn from './pages/Signin'
@@ -33,10 +34,24 @@ const router = createBrowserRouter([{
 ]
 },])
 
+=======
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+>>>>>>> a8ad40e (commit after creating git routes)
 function App() {
    return (
     <div>
+<<<<<<< HEAD
         <RouterProvider router = {router} />
+=======
+        <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element = {<Signup/>} />
+          <Route path='/signin' element = {<Signin/>} />
+          <Route path='/dashboard' element = {<Dashboard/>} />
+          <Route path='/send' element = {<Send/>} />
+        </Routes>
+        </BrowserRouter>
+>>>>>>> a8ad40e (commit after creating git routes)
     </div>
   )
 }
